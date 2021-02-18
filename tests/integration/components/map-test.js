819cc2d2d5711d861addafc13d3bdb9448cb7802
Kyungin Na <nakyungin93@gmail.com>
2021-02-18 17:48:31 +0100

    Adding a test to check if the src attribute is updated when arguments change

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -22,7 +22,7 @@ module('Integration | Component | map', function (hooks) {
       .hasAttribute('alt', 'Map image at coordinates 37.7797,-122.4184')
       .hasAttribute('src')
       .hasAttribute('width', '150')
-      .hasAttribute('height', '120')
+      .hasAttribute('height', '120');
 
     let { src } = find('.map img');
     let token = encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
@@ -38,6 +38,57 @@ module('Integration | Component | map', function (hooks) {
     assert.ok(
       src.includes(`access_token=${token}`),
       'the src should include the escaped access token'
+    );
+  });
+
+  test('it updates the "src" attribute when the arguments change', async function (assert) {
+    this.setProperties({
+      lat: 37.7797,
+      lng: -122.4184,
+      zoom: 10,
+      width: 150,
+      height: 120,
+    });
+    await render(hbs`<Map
+      @lat={{this.lat}}
+      @lng={{this.lng}}
+      @zoom={{this.zoom}}
+      @width={{this.width}}
+      @height={{this.height}}
+      alt="A map of San Francisco"
+     />`);
+
+    let img = find('.map img');
+
+    assert.ok(
+      img.src.includes('-122.4184,37.7797,10'),
+      'the src should include the lng,lat,zoom parameter'
+    );
+
+    assert.ok(
+      img.src.includes('150x120@2x'),
+      'the src should include the width,height and @2x parameter'
+    );
+
+    this.setProperties({
+      lat: 47.6062,
+      lng: -122.3321,
+      zoom: 12,
+    });
+
+    assert.ok(
+      img.src.includes('-122.3321,47.6062,12'),
+      'the src should include the new lng,lat,zoom parameter'
+    );
+
+    this.setProperties({
+      width: 300,
+      height: 200,
+    });
+
+    assert.ok(
+      img.src.includes('300x200@2x'),
+      'the src should include the new width,height and @2x parameter'
     );
   });
 
